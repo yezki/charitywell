cc51050b68c85a6d4f852e51dae95c029ce1cd57
Yeheizkiel Gabriel Batistuta <yeheizkielgb@gmail.com>
2025-02-05 20:40:23 +0700

    fix(navigation-menu): improve responsiveness of navigation link

--- src/components/layouts/navigation/Link.tsx
+++ src/components/layouts/navigation/Link.tsx
@@ -13,7 +13,7 @@ export const Link = ({
 }: LinkProps) => {
   return (
     <a href={href} className={cx(
-      "px-8 lg:px-6 py-2 text-sm lg:text-base text-center text-nowrap hover:text-black transition-colors",
+      "px-8 lg:px-3 xl:px-6 py-2 text-sm xl:text-base text-center text-nowrap hover:text-black transition-colors",
       (href === currentHash)
       ? "text-black font-bold" : "text-neutral-600",
     )}>
